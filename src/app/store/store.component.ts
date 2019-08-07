8e29933db3e761f0e1ad60be3c9e4989e1729a95
Rodolfo Rivas <rodolfo.rivas@gmail.com>
2019-08-07 02:11:23 -0500

    Arreglo de estilos

--- src/app/store/store.component.ts
+++ src/app/store/store.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-store',
@@ -7,7 +7,116 @@ import { Component, OnInit } from '@angular/core';
 })
 export class StoreComponent implements OnInit {
 
-  constructor() { }
+  listItem = [
+    {
+      img: '../../assets/img/cargador.jpg',
+      title: 'Cargador',
+      description: 'This is a wider card with supporting text below as a natural lead-in ' +
+        'to additional content.This content is a little bit longer.',
+      lastUpdate: 'Last updated 3 mins ago'
+    },
+    {
+      img: '../../assets/img/vidrios.jpg',
+      title: 'Vidrio',
+      description: 'This is a wider card with supporting text below as a natural lead-in ' +
+        'to additional content.This content is a little bit longer.',
+      lastUpdate: 'Last updated 3 mins ago'
+    }, {
+      img: '../../assets/img/vidrios.jpg',
+      title: 'Drone',
+      description: 'This is a wider card with supporting text below as a natural lead-in ' +
+        'to additional content.This content is a little bit longer.',
+      lastUpdate: 'Last updated 3 mins ago'
+    }, {
+      img: '../../assets/img/manos_llibres.png',
+      title: 'Manos libres',
+      description: 'This is a wider card with supporting text below as a natural lead-in ' +
+        'to additional content.This content is a little bit longer.',
+      lastUpdate: 'Last updated 3 mins ago'
+    },
+    {
+      img: '../../assets/img/cargador.jpg',
+      title: 'Cargador',
+      description: 'This is a wider card with supporting text below as a natural lead-in ' +
+        'to additional content.This content is a little bit longer.',
+      lastUpdate: 'Last updated 3 mins ago'
+    },
+    {
+      img: '../../assets/img/vidrios.jpg',
+      title: 'Vidrio',
+      description: 'This is a wider card with supporting text below as a natural lead-in ' +
+        'to additional content.This content is a little bit longer.',
+      lastUpdate: 'Last updated 3 mins ago'
+    }, {
+      img: '../../assets/img/vidrios.jpg',
+      title: 'Drone',
+      description: 'This is a wider card with supporting text below as a natural lead-in ' +
+        'to additional content.This content is a little bit longer.',
+      lastUpdate: 'Last updated 3 mins ago'
+    }, {
+      img: '../../assets/img/manos_llibres.png',
+      title: 'Manos libres',
+      description: 'This is a wider card with supporting text below as a natural lead-in ' +
+        'to additional content.This content is a little bit longer.',
+      lastUpdate: 'Last updated 3 mins ago'
+    },
+    {
+      img: '../../assets/img/cargador.jpg',
+      title: 'Cargador',
+      description: 'This is a wider card with supporting text below as a natural lead-in ' +
+        'to additional content.This content is a little bit longer.',
+      lastUpdate: 'Last updated 3 mins ago'
+    },
+    {
+      img: '../../assets/img/vidrios.jpg',
+      title: 'Vidrio',
+      description: 'This is a wider card with supporting text below as a natural lead-in ' +
+        'to additional content.This content is a little bit longer.',
+      lastUpdate: 'Last updated 3 mins ago'
+    }, {
+      img: '../../assets/img/vidrios.jpg',
+      title: 'Drone',
+      description: 'This is a wider card with supporting text below as a natural lead-in ' +
+        'to additional content.This content is a little bit longer.',
+      lastUpdate: 'Last updated 3 mins ago'
+    }, {
+      img: '../../assets/img/manos_llibres.png',
+      title: 'Manos libres',
+      description: 'This is a wider card with supporting text below as a natural lead-in ' +
+        'to additional content.This content is a little bit longer.',
+      lastUpdate: 'Last updated 3 mins ago'
+    },
+    {
+      img: '../../assets/img/cargador.jpg',
+      title: 'Cargador',
+      description: 'This is a wider card with supporting text below as a natural lead-in ' +
+        'to additional content.This content is a little bit longer.',
+      lastUpdate: 'Last updated 3 mins ago'
+    },
+    {
+      img: '../../assets/img/vidrios.jpg',
+      title: 'Vidrio',
+      description: 'This is a wider card with supporting text below as a natural lead-in ' +
+        'to additional content.This content is a little bit longer.',
+      lastUpdate: 'Last updated 3 mins ago'
+    }, {
+      img: '../../assets/img/vidrios.jpg',
+      title: 'Drone',
+      description: 'This is a wider card with supporting text below as a natural lead-in ' +
+        'to additional content.This content is a little bit longer.',
+      lastUpdate: 'Last updated 3 mins ago'
+    }, {
+      img: '../../assets/img/manos_llibres.png',
+      title: 'Manos libres',
+      description: 'This is a wider card with supporting text below as a natural lead-in ' +
+        'to additional content.This content is a little bit longer.',
+      lastUpdate: 'Last updated 3 mins ago'
+    }
+
+  ];
+
+  constructor() {
+  }
 
   ngOnInit() {
   }
